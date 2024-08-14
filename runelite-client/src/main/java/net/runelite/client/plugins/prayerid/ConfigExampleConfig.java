0c91a73c2b98951b8ea4d75d712be6e4f88c8526
Tim Zhong <timzhong@alum.mit.edu>
2024-08-14 18:24:38 -0400

    Creating starting files

--- runelite-client/src/main/java/net/runelite/client/plugins/prayerid/ConfigExampleConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerid/ConfigExampleConfig.java
@@ -5,8 +5,44 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
+import java.awt.*;
+
 @ConfigGroup("prayerid")
 public interface ConfigExampleConfig extends Config
 {
     // Configuration menu items go here
+
+    @ConfigItem(
+            position = 1,
+            keyName = "booleanConfig",
+            name = "Boolean checkbox",
+            description = "Simple example of a checkbox to store a booleans value"
+    )
+    default boolean booleanConfig() {return false;}
+
+    @ConfigItem(
+            position = 2,
+            keyName = "intConfig",
+            name = "Int Spinner",
+            description = "Simple example of a spinner to store integers"
+    )
+    default int intConfig() { return 1; }
+
+    @ConfigItem(
+            position = 3,
+            keyName = "stringConfig",
+            name = "String Textbox",
+            description = "Simple example of a tesxtbox to store strings"
+    )
+    default String stringConfig() {
+        return "string";
+    }
+
+    @ConfigItem(
+            position = 4,
+            keyName = "colorConfig",
+            name = "Color Selector",
+            description = "Simple example of a color selector"
+    )
+    default Color colorConfig() {return Color.BLACK;}
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/prayerid/ConfigExamplePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerid/ConfigExamplePlugin.java
@@ -5,6 +5,18 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.plugins.Plugin;
+import javax.inject.Inject;
+import net.runelite.client.plugins.PluginDescriptor;
 
+@PluginDescriptor(
+        name = "Config example",
+        description = "A simple example plugin to illustrate how to write plugin config menus",
+        tags = {"config", "menu"},
+        loadWhenOutdated = true,
+        enabledByDefault = false
+)
 public class ConfigExamplePlugin extends Plugin {
+
+    @Inject
+    private ConfigExampleConfig config;
 }

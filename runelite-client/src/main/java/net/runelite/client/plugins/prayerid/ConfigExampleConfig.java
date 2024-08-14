54bf8cc0004583b45a64e1fe4b0a339c53161894
Tim Zhong <timzhong@alum.mit.edu>
2024-08-14 14:21:08 -0400

    Creating starting files

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerid/ConfigExampleConfig.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.prayerid;
+
+// Mandatory imports
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("prayerid")
+public interface ConfigExampleConfig extends Config
+{
+    // Configuration menu items go here
+}
---
+++ runelite-client/src/main/java/net/runelite/client/plugins/prayerid/ConfigExamplePlugin.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.prayerid;
+
+// Mandatory imports
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.plugins.Plugin;
+
+public class ConfigExamplePlugin extends Plugin {
+}
